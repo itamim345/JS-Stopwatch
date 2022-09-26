82529a5d420abc090c7fa0dbd9d46db132da0a66
AF Tamim <itamim345@gmail.com>
2022-09-26 16:03:23 +0530

    Added extra countdown

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 let min = 0;
 let sec = 0;
+let msec = 0;
 let interVal;
 let getMin = document.getElementById('min');
 let getSec = document.getElementById('sec');
+let getMsec = document.getElementById('m-sec');
 let startBtn = document.getElementById("start");
 let stopBtn = document.getElementById("stop");
 let resetBtn = document.getElementById("reset");
 
 // Start with Event Listener
 startBtn.addEventListener('click', ()=>{
-   interVal = setInterval(timerStart,1000);    
+   interVal = setInterval(timerStart,10);    
 })
 
 // Stop with Event Listener
@@ -27,13 +29,25 @@ resetBtn.addEventListener('click', ()=>{
 })
 
 function timerStart(){
-    sec++;
-    if(sec <= 9){
+    //For Msec
+    msec++;
+    if(msec <= 9){
+        getMsec.innerHTML = '0' + msec;
+    }
+    if(msec > 9){
+        getMsec.innerHTML = msec;
+    }
+    //For Sec
+    if(msec >= 60){
+        sec ++;
         getSec.innerHTML = '0' + sec;
+        msec = 0;
+        getMsec.innerHTML = '0' + msec;
     }
     if(sec > 9){
         getSec.innerHTML = sec;
     }
+    //For Min
     if(sec >= 60){
         min ++;
         getMin.innerHTML = '0' + min;
